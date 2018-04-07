d62b87a9bc3841439a2be4d226be364d6604b87a
Johnny29 <zero998877@gmail.com>
2018-04-07 21:20:52 +0800

    test header done

--- src/GSTest.java
+++ src/GSTest.java
@@ -25,7 +25,7 @@ class GSTest {
 	@BeforeEach
 	void setUp() throws Exception {
 		gradeSys = new GradeSys();
-		Scanner scanner = new Scanner(new File("src/simple.txt"));
+		Scanner scanner = new Scanner(new File("src/gradeinput.txt"));
 		while(scanner.hasNext()) {
 			String studentData= scanner.nextLine();
 			StudentsGrade studentsGrade = new StudentsGrade(studentData.split(" "));
@@ -39,6 +39,14 @@ class GSTest {
 		gradeSys = null;
 	}
 	
+	/** ----------------------------------------------------------------------------------------------------------------------
+	Unit test
+	testAdd()
+
+	case 1: "104062104 吳宗欣 100 100 100 100 100"
+	case 2: "104062103 李宗翰 70 70 60 100 90"
+	------------------------------------------------------------------------------------------------------------------------- */
+
 	@Test
 	void testAdd() throws Exception{
 		String newInput = "104062104 吳宗欣 100 100 100 100 100";
@@ -47,10 +55,24 @@ class GSTest {
 		gradeSys.add(newCase);
 		assertEquals(true, gradeSys.inList(newCase.getID()));
 	}
-	
+	@Test
+	void testAdd2() throws Exception{
+		String newInput = "104062103 李宗翰 70 70 60 100 90";
+		StudentsGrade newCase = new StudentsGrade(newInput.split(" "));
+		assertEquals(false, gradeSys.inList(newCase.getID()));
+		gradeSys.add(newCase);
+		assertEquals(true, gradeSys.inList(newCase.getID()));
+	}
+	/** ----------------------------------------------------------------------------------------------------------------------
+	Unit test
+	testInList()
+
+	case 1: "104062104 "
+	case 2: "104062103 "
+	------------------------------------------------------------------------------------------------------------------------- */
 	@Test
 	void testInList() throws Exception{
-		Scanner scanner = new Scanner(new File("src/simple.txt"));
+		Scanner scanner = new Scanner(new File("src/gradeinput.txt"));
 		while(scanner.hasNext()) {
 			String studentData= scanner.nextLine();
 			Long ID = Long.parseLong(studentData.split(" ")[0]);
@@ -59,10 +81,26 @@ class GSTest {
 		scanner.close();
 		assertEquals(false, gradeSys.inList(new Long(104062104)));
 	}
-	
+	@Test
+	void testInList2() throws Exception{
+		Scanner scanner = new Scanner(new File("src/gradeinput.txt"));
+		while(scanner.hasNext()) {
+			String studentData= scanner.nextLine();
+			Long ID = Long.parseLong(studentData.split(" ")[0]);
+			assertEquals(true, gradeSys.inList(ID));
+		}
+		scanner.close();
+		assertEquals(false, gradeSys.inList(new Long(104062103)));
+	}
+	/** ----------------------------------------------------------------------------------------------------------------------
+	Unit test
+	testGetGrade()
+
+	case 1: "src/gradeinput.txt"
+	------------------------------------------------------------------------------------------------------------------------- */
 	@Test
 	void testGetGrade() throws Exception{
-		Scanner scanner = new Scanner(new File("src/simple.txt"));
+		Scanner scanner = new Scanner(new File("src/gradeinput.txt"));
 		while(scanner.hasNext()) {
 			String input= scanner.nextLine();
 			String studentData[] = input.split(" ");
@@ -75,11 +113,16 @@ class GSTest {
 		}
 		scanner.close();
 	}
-	
+	/** ----------------------------------------------------------------------------------------------------------------------
+	Unit test
+	testGetName()
+
+	case 1: "src/gradeinput.txt"
+	------------------------------------------------------------------------------------------------------------------------- */
 	
 	@Test
 	void testGetName() throws Exception{
-		Scanner scanner = new Scanner(new File("src/simple.txt"));
+		Scanner scanner = new Scanner(new File("src/gradeinput.txt"));
 		while(scanner.hasNext()) {
 			String input= scanner.nextLine();
 			String studentData[] = input.split(" ");
@@ -88,10 +131,15 @@ class GSTest {
 		}
 		scanner.close();
 	}
-	
+	/** ----------------------------------------------------------------------------------------------------------------------
+	Unit test
+	testGetNumberOfMember()
+
+	case 1: "src/gradeinput.txt"
+	------------------------------------------------------------------------------------------------------------------------- */
 	@Test
 	void testGetNumberOfMember() throws Exception{
-		Scanner scanner = new Scanner(new File("src/simple.txt"));
+		Scanner scanner = new Scanner(new File("src/gradeinput.txt"));
 		int i = 0;
 		while(scanner.hasNextLine()) {
 			scanner.nextLine();
@@ -100,13 +148,24 @@ class GSTest {
 		scanner.close();
 		assertEquals(i, gradeSys.getNumberOfMember());
 	}
+	/** ----------------------------------------------------------------------------------------------------------------------
+	Unit test
+	testGetWeight()
 
+	case 1:{10,10,10,30,40}
+	------------------------------------------------------------------------------------------------------------------------- */
 	@Test
 	void testGetWeight() throws Exception{
 		float expect[] = {10,10,10,30,40};
 		assertArrayEquals(expect, gradeSys.getWeight());
 	}
-	
+	/** ----------------------------------------------------------------------------------------------------------------------
+	Unit test
+	testUpdateWeight()
+
+	case 1:{20,20,20,20,20}
+	case 2:{10,20,30,20,20}
+	------------------------------------------------------------------------------------------------------------------------- */
 	@Test
 	void testUpdateWeight() throws Exception{
 		float expect[] = {10,10,10,30,40};
@@ -115,67 +174,63 @@ class GSTest {
 		gradeSys.updateWeight(newWeight);
 		assertArrayEquals(newWeight, gradeSys.getWeight());
 	}
-	
+	@Test
+	void testUpdateWeight2() throws Exception{
+		float expect[] = {10,10,10,30,40};
+		assertArrayEquals(expect, gradeSys.getWeight());
+		float newWeight[] = {10,20,30,20,20};
+		gradeSys.updateWeight(newWeight);
+		assertArrayEquals(newWeight, gradeSys.getWeight());
+	}
+	/** ----------------------------------------------------------------------------------------------------------------------
+	Unit test
+	testGetTotal()
+
+	case 1:955002056
+	case 2:962001044
+	------------------------------------------------------------------------------------------------------------------------- */
 	@Test
 	void testGetTotal() throws Exception{
-		Scanner scanner = new Scanner(new File("src/simple.txt"));
-		while(scanner.hasNext()) {
-			String input= scanner.nextLine();
-			String studentData[] = input.split(" ");
-			Long ID = Long.parseLong(studentData[0]);
-			int grade[] = new int[5];
-			for(int i=0;i<5;i++) {
-				grade[i] = Integer.parseInt(studentData[i+2]);
-			}
-			float weight[] = gradeSys.getWeight();
-			float total = 0;
-			for(int i=0;i<weight.length;i++) {
-				total+=weight[i]*(float)grade[i]/100;
-			}
-			if(Math.abs(total-gradeSys.getTotal(ID))>0.01)fail("");
-		}
-		scanner.close();
+		assertEquals(true,gradeSys.getTotal(new Long(955002056))-(float)((0.1*88)+(0.1*92)+(88*0.1)+(0.3*98)+(0.4*91))<0.001);
 	}
-	
+	@Test
+	void testGetTotal2() throws Exception{
+		assertEquals(true,gradeSys.getTotal(new Long(962001044))-(float)((0.1*87)+(0.1*86)+(98*0.1)+(0.3*88)+(0.4*87))<0.001);
+	}
+	/** ----------------------------------------------------------------------------------------------------------------------
+	Unit test
+	testGetRank()
+
+	case 1:955002056
+	case 2:962001044
+	------------------------------------------------------------------------------------------------------------------------- */
 	@Test
 	void testGetRank() throws Exception{
-		Long studentArray[] = new Long[gradeSys.getNumberOfMember()];
-		Scanner scanner = new Scanner(new File("src/simple.txt"));
-		while(scanner.hasNext()) {
-			String input= scanner.nextLine();
-			String studentData[] = input.split(" ");
-			Long ID = Long.parseLong(studentData[0]);
-			studentArray[gradeSys.getRank(ID)-1] = ID;
-		}
-		scanner.close();
-		for(int i=0;i<studentArray.length-1;i++) {
-			assertEquals(true, gradeSys.getTotal(studentArray[i])>=gradeSys.getTotal(studentArray[i+1]));
-		}
+		assertEquals(15, gradeSys.getRank(new Long(955002056)));
 	}
+	@Test
+	void testGetRank2() throws Exception{
+		assertEquals(37, gradeSys.getRank(new Long(962001044)));
+	}
+	/** ----------------------------------------------------------------------------------------------------------------------
+	Unit test
+	testGetAverage()
+	
+	case 1: "src/gradeinput.txt"
+	------------------------------------------------------------------------------------------------------------------------- */
 	
 	@Test
 	void testGetAverage() throws Exception{
-		Scanner scanner = new Scanner(new File("src/simple.txt"));
-		float total = 0;
-		float numOfMembers = 0;
-		while(scanner.hasNext()) {
-			String input= scanner.nextLine();
-			String studentData[] = input.split(" ");
-			Long ID = Long.parseLong(studentData[0]);
-			int grade[] = new int[5];
-			for(int i=0;i<5;i++) {
-				grade[i] = Integer.parseInt(studentData[i+2]);
-			}
-			float weight[] = {10,10,10,30,40};
-			for(int i=0;i<weight.length;i++) {
-				total+=weight[i]*(float)grade[i]/100;
-			}
-			numOfMembers++;
-		}
-		scanner.close();
-		if(Math.abs(total/numOfMembers-gradeSys.getAverage())>0.01)fail("");
+		assertEquals(true, gradeSys.getAverage()-89.46191<0.00001);
 	}
 	
+	/** ----------------------------------------------------------------------------------------------------------------------
+	Unit test
+	testShowGrades()
+	
+	case 1: lab1 81 lab2 32 lab3 50 mid-term 90 final exam 93 total 80.5
+	case 2: lab1 88 lab2 92 lab3 88 mid-term 98 final exam 91 total 92.6
+	------------------------------------------------------------------------------------------------------------------------- */
 
 	@Test
 	void testShowGrades() throws Exception{
@@ -199,30 +254,64 @@ class GSTest {
 				"Total grade:   92.6\n";
 		assertEquals(expect, stream.toString());
 	}
+	/** ----------------------------------------------------------------------------------------------------------------------
+	Unit test
+	testCaseUpdateWeight()
 	
+	case 1: 20% 20% 20% 20% 20%
+	case 2: 30% 20% 10% 20% 20%
+	------------------------------------------------------------------------------------------------------------------------- */
 	@Test
 	void testCaseUpdateWeight() throws Exception{
 		ByteArrayOutputStream stream = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(stream));
 		Scanner scanner = new Scanner("20\n20\n20\n20\n20\nY\n");
 		GradeSys.caseUpdateWeight(scanner, gradeSys);
-		String expect = "舊配分\r\n" + 
+		String expect = "舊配分\n" + 
 				"lab1:           10%\n" + 
 				"lab2:           10%\n" + 
 				"lab3:           10%\n" + 
 				"mid-term:       30%\n" + 
 				"final exam:     40%\n" + 
-				"輸入新配分\r\n" + 
-				"lab1:          lab2:          lab3:          mid-term:      final exam:    請確認新配分\r\n" + 
+				"輸入新配分\n" + 
+				"lab1:          lab2:          lab3:          mid-term:      final exam:    請確認新配分\n" + 
 				"lab1:           20%\n" + 
 				"lab2:           20%\n" + 
 				"lab3:           20%\n" + 
 				"mid-term:       20%\n" + 
 				"final exam:     20%\n" + 
-				"以上正確嗎? Y (Yes) 或 N (No)\r\n";
+				"以上正確嗎? Y (Yes) 或 N (No)\n";
 		assertEquals(expect, stream.toString());
 	}
+	@Test
+	void testCaseUpdateWeight2() throws Exception{
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(stream));
+		Scanner scanner = new Scanner("30\n20\n10\n20\n20\nY\n");
+		GradeSys.caseUpdateWeight(scanner, gradeSys);
+		String expect = "舊配分\n" + 
+				"lab1:           10%\n" + 
+				"lab2:           10%\n" + 
+				"lab3:           10%\n" + 
+				"mid-term:       30%\n" + 
+				"final exam:     40%\n" + 
+				"輸入新配分\n" + 
+				"lab1:          lab2:          lab3:          mid-term:      final exam:    請確認新配分\n" + 
+				"lab1:           30%\n" + 
+				"lab2:           20%\n" + 
+				"lab3:           10%\n" + 
+				"mid-term:       20%\n" + 
+				"final exam:     20%\n" + 
+				"以上正確嗎? Y (Yes) 或 N (No)\n";
+		assertEquals(expect, stream.toString());
+	}
+	/** ----------------------------------------------------------------------------------------------------------------------
+	Unit test
+	testMenu()
 	
+	case 1: 962001051
+	case 2: 955002056
+	------------------------------------------------------------------------------------------------------------------------- */
 	@Test
 	void testMenu1() throws Exception{
 		ByteArrayOutputStream stream = new ByteArrayOutputStream();
@@ -235,14 +324,14 @@ class GSTest {
 				"3)A 顯示平均\n" + 
 				"4)W 更新配分\n" + 
 				"5)E 離開選單\n" + 
-				">> 李威廷排名第63/63\r\n" + 
+				">> 李威廷排名第63/63\n" + 
 				"輸入指令 :\n" + 
 				"1)G 顯示成績\n" + 
 				"2)R 顯示排名\n" + 
 				"3)A 顯示平均\n" + 
 				"4)W 更新配分\n" + 
 				"5)E 離開選單\n" + 
-				">> 成績平均為: 89.46191\r\n" + 
+				">> 成績平均為: 89.46191\n" + 
 				"輸入指令 :\n" + 
 				"1)G 顯示成績\n" + 
 				"2)R 顯示排名\n" + 
@@ -265,14 +354,14 @@ class GSTest {
 				"3)A 顯示平均\n" + 
 				"4)W 更新配分\n" + 
 				"5)E 離開選單\n" + 
-				">> 許文馨排名第15/63\r\n" + 
+				">> 許文馨排名第15/63\n" + 
 				"輸入指令 :\n" + 
 				"1)G 顯示成績\n" + 
 				"2)R 顯示排名\n" + 
 				"3)A 顯示平均\n" + 
 				"4)W 更新配分\n" + 
 				"5)E 離開選單\n" + 
-				">> 成績平均為: 89.46191\r\n" + 
+				">> 成績平均為: 89.46191\n" + 
 				"輸入指令 :\n" + 
 				"1)G 顯示成績\n" + 
 				"2)R 顯示排名\n" + 
@@ -282,23 +371,50 @@ class GSTest {
 				">> ";
 		assertEquals(expect, stream.toString());
 	}
+	/** ----------------------------------------------------------------------------------------------------------------------
+	Unit test
+	testMenu()
 	
+	case 1: 0 962001051 E Q
+	case 2: k 955002056 E Q
+	------------------------------------------------------------------------------------------------------------------------- */
 	@Test
 	void testLogin() throws Exception{
 		ByteArrayOutputStream stream = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(stream));
 		Scanner scanner = new Scanner("0\n962001051\nE\nQ\n");
 		String expect = "輸入ID或Q(結束使用): " + 
-				"查無此ID!\r\n" + 
+				"查無此ID!\n" + 
 				"輸入ID或Q(結束使用): " + 
-				"Welcome 李威廷\r\n" + 
+				"Welcome 李威廷\n" + 
 				"輸入指令 :\n" + 
 				"1)G 顯示成績\n" + 
 				"2)R 顯示排名\n" + 
 				"3)A 顯示平均\n" + 
 				"4)W 更新配分\n" + 
 				"5)E 離開選單\n" + 
-				">> 輸入ID或Q(結束使用): ";
+				">> 輸入ID或Q(結束使用): "+
+				"結束程式\n";
+		GradeSys.login(scanner, gradeSys);
+		assertEquals(expect, stream.toString());
+	}
+	@Test
+	void testLogin2() throws Exception{
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(stream));
+		Scanner scanner = new Scanner("k\n955002056\nE\nQ\n");
+		String expect = "輸入ID或Q(結束使用): " + 
+				"輸入格式錯誤！\n" + 
+				"輸入ID或Q(結束使用): " + 
+				"Welcome 許文馨\n" + 
+				"輸入指令 :\n" + 
+				"1)G 顯示成績\n" + 
+				"2)R 顯示排名\n" + 
+				"3)A 顯示平均\n" + 
+				"4)W 更新配分\n" + 
+				"5)E 離開選單\n" + 
+				">> 輸入ID或Q(結束使用): "+
+				"結束程式\n";
 		GradeSys.login(scanner, gradeSys);
 		assertEquals(expect, stream.toString());
 	}
